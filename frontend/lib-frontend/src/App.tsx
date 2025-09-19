c9369eeffb5a4c292585cb43bda410a67ce2c58c
Sujan <157828203+s2002d@users.noreply.github.com>
2025-09-19 12:00:56 +0530

    Frontend part implement

---
+++ frontend/lib-frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const App: React.FC = () => {
+  return (
+    <div style={{ maxWidth: "600px", margin: "20px auto", fontFamily: "Arial" }}>
+      <h1>Library Management System</h1>
+      
+    </div>
+  );
+};
+
+export default App;
